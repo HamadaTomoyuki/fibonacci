7077b63e76565067de2f49e2fd3ab84bc98eac93
浜田 <stalemate1018@gmail.com>
2020-07-15 12:02:34 +0900

    トリボナッチ数列

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 'use strict';
+/*function tri(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 0;
+  } else if (n === 2) {
+    return 1;
+  }
+  return tri(n-3) + tri(n-2) +tri(n-1);
+}
+const length = 40;
+for (let i = 0; i<=length; i++) {
+  console.log(tri(i));
+}
+*/
+
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 0);
+memo.set(2, 1);
+
+function tri(n){
+  if (memo.has(n)){
+    return memo.get(n);
+  }
+  const value = tri(n-3) + tri(n-2) + tri(n-1);
+  memo.set(n, value);
+  return value;
+}
+const length = 40;
+for (let i = 0; i <= length; i++) {
+  console.log(tri(i));
+}
